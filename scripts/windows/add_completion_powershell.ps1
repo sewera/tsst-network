bb202393589357da6fa3463ced25ec5fba83f7b9
Blazej Sewera <code@sewera.dev>
2020-11-05 21:58:57 +0100

    #1 Add cli completion script for windows

---
+++ scripts/windows/add_completion_powershell.ps1
@@ -0,0 +1,11 @@
+if (!(Test-Path -Path $PROFILE)) {
+  New-Item -ItemType File -Path $PROFILE -Force
+}
+
+Add-Content $PROFILE "# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+     param($commandName, $wordToComplete, $cursorPosition)
+         dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+         }
+ }"
