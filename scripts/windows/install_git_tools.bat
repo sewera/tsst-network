c8f1b666ccf8cb2f063d49b839632a93b628f413
Blazej Sewera <code@sewera.dev>
2020-11-08 11:49:07 +0100

    #1 Fix bugs in shell scripts

--- scripts/windows/install_git_tools.bat
+++ scripts/windows/install_git_tools.bat
@@ -1,5 +1,6 @@
-echo off
+@echo off
 pip install pre-commit
 
 git config core.commentchar ';'
-pre-commit install -t {pre-commit,commit-msg}
+pre-commit install -t pre-commit
+pre-commit install -t commit-msg
