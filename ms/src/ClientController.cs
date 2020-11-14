e2fa66d03d7bd1d3daef3a8983cd9e6506eac3a1
AndreGolara <41gawor@gmail.com>
2020-11-14 21:55:29 +0100

    #25 Implement basic asynchronous socket server

---
+++ ms/src/ClientController.cs
@@ -0,0 +1,34 @@
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace ms
+{
+    /// <summary>
+    /// Class handling list with client connections
+    /// </summary>
+    static class ClientController
+    {
+        /// <summary>
+        /// List with client connections
+        /// </summary>
+        public static List<Client> Clients = new List<Client>();
+
+        /// <summary>
+        /// Add new client connection to the list
+        /// <param name="socket"> Socket associated with a specific client </param>
+        /// </summary>
+        public static void AddClient(Socket socket)
+        {
+            Clients.Add(new Client(socket, Clients.Count));
+        }
+        /// <summary>
+        /// Remove client connection from the list
+        /// <param name="id"> Id of client to be deleted </param>
+        /// </summary>
+        public static void RemoveClient(int id)
+        {
+            Clients.RemoveAt(Clients.FindIndex(x => x.Id == id));
+        }
+    }
+
+}
---
+++ ms/src/ManagementManager.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ms
+{
+    /// <summary>
+    /// ManagementManager implemented as server
+    /// </summary>
+    class ManagementManager : IManagementManager
+    {
+        /// <summary>
+        /// This socket will listen for incoming connections
+        /// </summary>
+        public Socket ListenerSocket;
+        
+        /// <summary>
+        /// Port on which socket will listen
+        /// </summary>
+        public short port = 1234;
+
+        /// <summary>
+        /// Class contructor initializing ListenerSocket
+        /// </summary>
+        public ManagementManager()
+        {
+            ListenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        /// <summary>
+        /// This method starts the process of listening for incoming connections from clients - network nodes
+        /// </summary>
+        public void startListening()
+        {
+            try
+            {
+                Console.WriteLine($"Listening started port: {port}");
+                // Bind socket to the IPEndPoint, class representing a network endpoint as an IP address and a port number
+                ListenerSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+                // 10 is then number of incoming connection that can be queued up for acceptance
+                ListenerSocket.Listen(10);
+                // The server will start listening for incoming connections and will go on with other logic. When there is an
+                // connection the server switches back to this method and will run the AcceptCallBack method
+                ListenerSocket.BeginAccept(AcceptCallback, ListenerSocket);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Listening error" + ex);
+            }
+        }
+
+        /// <summary>
+        /// This method is called when new connection popped up and needs to be serviced
+        /// <summary> 
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Console.WriteLine($"Accept Callback port:{port}");
+                // Create new socket for client
+                Socket acceptedSocket = ListenerSocket.EndAccept(ar);
+                // Add new client to ClientController
+                ClientController.AddClient(acceptedSocket);
+
+                // Start listening again
+                ListenerSocket.BeginAccept(AcceptCallback, ListenerSocket);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Base Accept error" + ex);
+            }
+        }
+    }
+}
