7de1d658fb288045235187391e6796860cbd90e5
batmatt <m.w.1999@o2.pl>
2020-11-14 19:35:08 +0100

    #24 Add directories for Models and Utils classes, implement method for entering destAddress and message

--- cn/src/ClientNode.cs
+++ cn/src/ClientNode.cs
@@ -1,12 +1,28 @@
-﻿using System;
+using System;
+using System.Net;
+using cn.Models;
+using cn.Utils;
 
 namespace cn
 {
     class ClientNode
     {
+        /// <summary>
+        /// IP address of message receiver
+        /// </summary>
+        private IPAddress destinationAddress { get; set; }
+
+        /// <summary>
+        /// User's input message
+        /// </summary>
+        private string message { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MplsPacket packet = new MplsPacket();
+            IUserInterface ui = new UserInterface();
+
+            ui.EnterDestAddressAndMessage();
         }
     }
 }
