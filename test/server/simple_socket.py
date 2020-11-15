cc6c38d737f45b04466849a0789869b76978ee9a
Blazej Sewera <code@sewera.dev>
2020-11-15 15:30:17 +0100

    #26 Add simple python socket

---
+++ test/server/simple_socket.py
@@ -0,0 +1,23 @@
+import socket
+
+ADDRESS = 'localhost'
+PORT = 7357
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind((ADDRESS, PORT))
+serversocket.listen(5)  # become a server socket, maximum 5 connections
+
+print(f'Listening on {ADDRESS}:{PORT}. Ctrl-C to quit')
+
+while True:
+    try:
+        connection, address = serversocket.accept()
+        buf = connection.recv(64)
+        if len(buf) > 0:
+            print(buf)
+            buf = b''
+    except KeyboardInterrupt:
+        print()
+        print('Closing socket')
+        serversocket.close()
+        exit(0)
