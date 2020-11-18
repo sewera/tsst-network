a019e947cbf06ee9ded3fbc0dffa697e01631621
AndreGolara <41gawor@gmail.com>
2020-11-18 17:41:28 +0100

    #25 Add Log package to the project

--- ms/src/ManagementSystem.cs
+++ ms/src/ManagementSystem.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Threading;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace ms
 {
     class ManagementSystem
     {
+        private static readonly Logger LOG = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            LoggerSetup();
             Config config = new Config();
             config.ReadConfigFile("ManagementSystem.xml");
             IManagementManager mm = new ManagementManager();
@@ -14,8 +19,19 @@ namespace ms
             mm.startListening();
             MessageSender.ReadConfig(config);
             new Thread(MessageSender.Start).Start();
-            
             UserInterface.Start();
+        }
+
+        private static void LoggerSetup()
+        {
+            LoggingConfiguration config = new LoggingConfiguration();
+            ColoredConsoleTarget consoleTarget = new ColoredConsoleTarget
+            {
+                Name = "console",
+                Layout = "[${time} | ${level:format=FirstCharacter} | ${logger}] ${message}"
+            };
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, consoleTarget);
+            LogManager.Configuration = config;
         }
     }
 }
