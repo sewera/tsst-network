cdd7c6000b12ab529bd3bb464a2b0b68ff4866d8
AndreGolara <41gawor@gmail.com>
2020-11-25 17:35:05 +0100

    #25 Send commands as ManagementPackets

--- ms/src/Networking/Senders/SenderServer.cs
+++ ms/src/Networking/Senders/SenderServer.cs
@@ -19,33 +19,29 @@ namespace ms
             _sendSocked = sendSocket;
         }
         /// <summary>
-        /// Send data as string
+        /// Send data as ManagementPacket 
         /// <param name="data"> String to be sent </param>
         /// <summary>
         public void Send(string data)
         {
+            List<string> words = new List<string>(data.Split(' '));
+            words.RemoveAll(item => item == "");
+            string commandType = words[0];
+            words.RemoveAt(0);
+            string commandData = string.Join(' ',words);
+
             try
             {
-                /* what happens here:
-                1. Create a list of bytes
-                2. Add the length of the string to the list.
-                So if this message arrives at the server we can easily read the length of the
-                coming message.
-                3. Add the message(string) bytes
-                */
-                // 1.
-                var fullPacket = new List<byte>();
-                // 2.
-                fullPacket.AddRange(BitConverter.GetBytes(data.Length));
-                // 3.
-                fullPacket.AddRange(Encoding.Default.GetBytes(data));
-                /* Send the message to the server we are currently connected to.
-                Or package stucture is {length of data 4 bytes (int32), actual data}*/
-                _sendSocked.Send(fullPacket.ToArray());
+                // Create ManagementPacket
+                ManagementPacket packet = new ManagementPacket.Builder()
+                        .SetCommandType(commandType)
+                        .SetCommandData(commandData) 
+                        .Build();
+                    _sendSocked.Send(packet.ToBytes());
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                throw new Exception($"Sending error {ex}");
+                throw new Exception($"Sending error {e}");
             }
         }
     }
