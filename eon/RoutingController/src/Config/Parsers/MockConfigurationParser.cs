19fa781b0b13acb90ad449e529ae21656f81482b
batmatt <m.w.1999@o2.pl>
2021-01-28 17:42:04 +0100

    #87 Add config files and XmlParser in RoutingController

--- eon/RoutingController/src/Config/Parsers/MockConfigurationParser.cs
+++ eon/RoutingController/src/Config/Parsers/MockConfigurationParser.cs
@@ -10,6 +10,11 @@ namespace RoutingController.Config.Parsers
                 .SetLocalTopologyLocalPort(6011)
                 .SetNetworkTopologyLocalPort(6012)
                 .SetRouteTableQueryLocalPort(6013)
+                .AddRouteTableRow(new Configuration.RouteTableRow.RouteTableRowBuilder()
+                    .SetSrc("1xx")
+                    .SetDst("122")
+                    .SetGateway("115")
+                    .Build())
                 .Build();
         }
     }
