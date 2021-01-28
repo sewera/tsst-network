eb806aa115880801dec5f68b667262ff15dc19b1
AndreGolara <41gawor@gmail.com>
2021-01-28 23:45:24 +0100

    #92 commit current work

--- eon/ConnectionController/src/ConnectionControllerStateDomain.cs
+++ eon/ConnectionController/src/ConnectionControllerStateDomain.cs
@@ -1,12 +1,24 @@
-﻿using Common.Models;
+﻿using System.Collections.Generic;
+using System.Net;
+using Common.Api;
+using Common.Models;
+using NLog;
 
 namespace ConnectionController
 {
     public class ConnectionControllerStateDomain : IConnectionControllerState
     {
-        public ConnectionControllerStateDomain() // Add things from config
+        private static readonly Logger LOG = LogManager.GetCurrentClassLogger();
+        
+        private readonly IApiClient<RequestPacket, ResponsePacket> _ccPeerCoordinationClient;
+        private readonly Dictionary<string, IApiClient<RequestPacket, ResponsePacket>> _ccConnectionRequestClients;
+        public ConnectionControllerStateDomain(int ccConnectionRequestRemotePort, 
+                                               int ccPeerCoordinationRemotePort,
+                                               IPAddress serverAddress
+                                               ) // Add things from config
         {
-            
+            _ccPeerCoordinationClient =
+                new ApiClient<RequestPacket, ResponsePacket>(serverAddress, ccPeerCoordinationRemotePort);
         }
 
         public ResponsePacket OnConnectionRequest(RequestPacket requestPacket)
