26e8b14ab60605a61fb0edf716733cf6882df4bc
Blazej Sewera <blazejok1@wp.pl>
2021-01-28 18:17:09 +0100

    #86 Implement basic Cpcc interfaces and connect them to the ui

--- eon/ClientNode/src/Ui/Parsers/ICommandParser.cs
+++ eon/ClientNode/src/Ui/Parsers/ICommandParser.cs
@@ -4,6 +4,8 @@ namespace ClientNode.Ui.Parsers
 {
     public interface ICommandParser
     {
+        (string, (int, int)) ParseCpccCommand(string command);
+
         (string, string) ParseCommand(string command);
 
         (List<long>, string) CheckMplsOutLabel(string mplsOutLabel);
