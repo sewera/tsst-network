0dc7431eb925a49648a6360a5c71b1abd15d29c0
Blazej Sewera <blazejok1@wp.pl>
2021-01-28 12:58:59 +0100

    #82 Start script rewrite

--- eon/start.py
+++ eon/start.py
@@ -12,6 +12,11 @@ parser.add_argument(
     action="store_true",
     help="Build solution with Release configuration",
 )
+parser.add_argument(
+    "--forcewindowsimpl",
+    action="store_true",
+    help="Force the Windows implementation of the script",
+)
 args = parser.parse_args()
 
 project_root = Path(__file__).parent
@@ -19,44 +24,87 @@ config_dir = Path("resources/config")
 exec_dir = Path("bin/Release/netcoreapp3.1")
 log_dir = Path("logs")
 
-cc = Path("CableCloud")
-cc_csproj = Path("CableCloud.csproj")
-cc_config = Path("CableCloud.xml")
-cc_exe = Path("CableCloud.exe")
+CableCloud = Path("CableCloud")
+CableCloud_csproj = Path("CableCloud.csproj")
+CableCloud_config = Path("CableCloud.xml")
+CableCloud_exe = Path("CableCloud.exe")
 
-cn = Path("ClientNode")
-cn_csproj = Path("ClientNode.csproj")
-cn_configs = [
+ClientNode = Path("ClientNode")
+ClientNode_csproj = Path("ClientNode.csproj")
+ClientNode_configs = [
     Path("ClientNode1.xml"),
     Path("ClientNode2.xml"),
     Path("ClientNode3.xml"),
     Path("ClientNode4.xml"),
 ]
-cn_exe = Path("ClientNode.exe")
+ClientNode_exe = Path("ClientNode.exe")
 
-nn = Path("NetworkNode")
-nn_csproj = Path("NetworkNode.csproj")
-nn_configs = [
-    Path("NetworkNode1.xml"),
-    Path("NetworkNode2.xml"),
-    Path("NetworkNode3.xml"),
-    Path("NetworkNode4.xml"),
-    Path("NetworkNode5.xml"),
-    Path("NetworkNode6.xml"),
-    Path("NetworkNode7.xml"),
+NetworkNode = Path("NetworkNode")
+NetworkNode_csproj = Path("NetworkNode.csproj")
+NetworkNode_configs = [
+    Path("NetworkNodeR01.xml"),
+    Path("NetworkNodeR02.xml"),
+    Path("NetworkNodeR03.xml"),
+    Path("NetworkNodeR11.xml"),
+    Path("NetworkNodeR12.xml"),
+    Path("NetworkNodeR13.xml"),
+    Path("NetworkNodeR14.xml"),
+    Path("NetworkNodeR21.xml"),
+    Path("NetworkNodeR22.xml"),
+    Path("NetworkNodeR23.xml"),
+    Path("NetworkNodeR31.xml"),
+    Path("NetworkNodeR32.xml"),
 ]
-nn_exe = Path("NetworkNode.exe")
+NetworkNode_exe = Path("NetworkNode.exe")
 
-if os.name == "nt":
+ConnectionController = Path("ConnectionController")
+ConnectionController_csproj = Path("ConnectionController.csproj")
+ConnectionController_configs = [
+    Path("ConnectionControllerR11.xml"),
+    Path("ConnectionControllerS1.xml"),
+    Path("ConnectionControllerSN1.xml"),
+]
+ConnectionController_exe = Path("ConnectionController.exe")
+
+NetworkCallController = Path("NetworkCallController")
+NetworkCallController_csproj = Path("NetworkCallController.csproj")
+NetworkCallController_configs = [
+    Path("NetworkCallControllerS1.xml"),
+    Path("NetworkCallControllerS2.xml"),
+]
+NetworkCallController_exe = Path("NetworkCallController.exe")
+
+RoutingController = Path("RoutingController")
+RoutingController_csproj = Path("RoutingController.csproj")
+RoutingController_configs = [
+    Path("RoutingControllerS1.xml"),
+    Path("RoutingControllerS2.xml"),
+    Path("RoutingControllerSN0.xml"),
+    Path("RoutingControllerSN1.xml"),
+    Path("RoutingControllerSN2.xml"),
+    Path("RoutingControllerSN3.xml"),
+]
+RoutingController_exe = Path("RoutingController.exe")
+
+if os.name == "nt" or args.forcewindowsimpl:
     if args.build:
         os.system(
-            f'start cmd.exe /k "dotnet build -c Release {(project_root/cc/cc_csproj).absolute()}"'
+            f'start cmd.exe /k "dotnet build -c Release {(project_root/CableCloud/CableCloud_csproj).absolute()}"'
         )
         os.system(
-            f'start cmd.exe /k "dotnet build -c Release {(project_root/nn/nn_csproj).absolute()}"'
+            f'start cmd.exe /k "dotnet build -c Release {(project_root/NetworkNode/NetworkNode_csproj).absolute()}"'
         )
         os.system(
-            f'start cmd.exe /k "dotnet build -c Release {(project_root/cn/cn_csproj).absolute()}"'
+            f'start cmd.exe /k "dotnet build -c Release {(project_root/ClientNode/ClientNode_csproj).absolute()}"'
+        )
+        os.system(
+            f'start cmd.exe /k "dotnet build -c Release {(project_root/ConnectionController/ConnectionController_csproj).absolute()}"'
+        )
+        os.system(
+            f'start cmd.exe /k "dotnet build -c Release {(project_root/NetworkCallController/NetworkCallController_csproj).absolute()}"'
+        )
+        os.system(
+            f'start cmd.exe /k "dotnet build -c Release {(project_root/RoutingController/RoutingController_csproj).absolute()}"'
         )
         exit(0)
 
@@ -66,30 +114,54 @@ if os.name == "nt":
         exit(0)
 
     os.system(
-        f'start cmd.exe /k "{(project_root/cc/exec_dir/cc_exe).absolute()} -c {(project_root/cc/config_dir/cc_config).absolute()} -l {(project_root/log_dir).absolute()}"'
+        f'start cmd.exe /k "{(project_root/CableCloud/exec_dir/CableCloud_exe).absolute()} -c {(project_root/CableCloud/config_dir/CableCloud_config).absolute()} -l {(project_root/log_dir).absolute()}"'
     )
+
+    for rc_config in RoutingController_configs:
+        os.system(
+            f'start cmd.exe /k "{(project_root/RoutingController/exec_dir/RoutingController_exe).absolute()} -c {(project_root/RoutingController/config_dir/rc_config).absolute()} -l {(project_root/log_dir).absolute()}"'
+        )
 
     time.sleep(2)
 
-    for nn_config in nn_configs:
+    for ncc_config in NetworkCallController_configs:
         os.system(
-            f'start cmd.exe /k "{(project_root/nn/exec_dir/nn_exe).absolute()} -c {(project_root/nn/config_dir/nn_config).absolute()} -l {(project_root/log_dir).absolute()}"'
+            f'start cmd.exe /k "{(project_root/NetworkCallController/exec_dir/NetworkCallController_exe).absolute()} -c {(project_root/NetworkCallController/config_dir/ncc_config).absolute()} -l {(project_root/log_dir).absolute()}"'
         )
 
-    for cn_config in cn_configs:
+    for cc_config in ConnectionController_configs:
         os.system(
-            f'start cmd.exe /k "{(project_root/cn/exec_dir/cn_exe).absolute()} -c {(project_root/cn/config_dir/cn_config).absolute()} -l {(project_root/log_dir).absolute()}"'
+            f'start cmd.exe /k "{(project_root/ConnectionController/exec_dir/ConnectionController_exe).absolute()} -c {(project_root/ConnectionController/config_dir/cc_config).absolute()} -l {(project_root/log_dir).absolute()}"'
+        )
+
+    for nn_config in NetworkNode_configs:
+        os.system(
+            f'start cmd.exe /k "{(project_root/NetworkNode/exec_dir/NetworkNode_exe).absolute()} -c {(project_root/NetworkNode/config_dir/nn_config).absolute()} -l {(project_root/log_dir).absolute()}"'
+        )
+
+    for cn_config in ClientNode_configs:
+        os.system(
+            f'start cmd.exe /k "{(project_root/ClientNode/exec_dir/ClientNode_exe).absolute()} -c {(project_root/ClientNode/config_dir/cn_config).absolute()} -l {(project_root/log_dir).absolute()}"'
         )
 else:
     if args.build:
         os.system(
-            f'{os.getenv("TERM")} -e dotnet build -c Release {(project_root/cc/cc_csproj).absolute()}'
+            f'{os.getenv("TERM")} -e dotnet build -c Release {(project_root/CableCloud/CableCloud_csproj).absolute()}'
         )
         os.system(
-            f'{os.getenv("TERM")} -e dotnet build -c Release {(project_root/nn/nn_csproj).absolute()}'
+            f'{os.getenv("TERM")} -e dotnet build -c Release {(project_root/NetworkNode/NetworkNode_csproj).absolute()}'
         )
         os.system(
-            f'{os.getenv("TERM")} -e dotnet build -c Release {(project_root/cn/cn_csproj).absolute()}'
+            f'{os.getenv("TERM")} -e dotnet build -c Release {(project_root/ClientNode/ClientNode_csproj).absolute()}'
+        )
+        os.system(
+            f'{os.getenv("TERM")} -e dotnet build -c Release {(project_root/ConnectionController/ConnectionController_csproj).absolute()}'
+        )
+        os.system(
+            f'{os.getenv("TERM")} -e dotnet build -c Release {(project_root/NetworkCallController/NetworkCallController_csproj).absolute()}'
+        )
+        os.system(
+            f'{os.getenv("TERM")} -e dotnet build -c Release {(project_root/RoutingController/RoutingController_csproj).absolute()}'
         )
         exit(0)
 
@@ -97,20 +169,38 @@ else:
         os.system("killall ClientNode")
         os.system("killall NetworkNode")
         os.system("killall CableCloud")
+        os.system("killall ConnectionController")
+        os.system("killall NetworkCallController")
+        os.system("killall RoutingController")
         exit(0)
 
     os.popen(
-        f'{os.getenv("TERM")} -e {(project_root/cc/exec_dir/cc).absolute()} -c {(project_root/cc/config_dir/cc_config).absolute()} -l {(project_root/log_dir).absolute()}'
+        f'{os.getenv("TERM")} -e {(project_root/CableCloud/exec_dir/CableCloud).absolute()} -c {(project_root/CableCloud/config_dir/CableCloud_config).absolute()} -l {(project_root/log_dir).absolute()}'
     )
+
+    for rc_config in RoutingController_configs:
+        os.popen(
+            f'{os.getenv("TERM")} -e {(project_root/RoutingController/exec_dir/RoutingController).absolute()} -c {(project_root/RoutingController/config_dir/rc_config).absolute()} -l {(project_root/log_dir).absolute()}'
+        )
 
     time.sleep(2)
 
-    for nn_config in nn_configs:
+    for ncc_config in NetworkCallController_configs:
         os.popen(
-            f'{os.getenv("TERM")} -e {(project_root/nn/exec_dir/nn).absolute()} -c {(project_root/nn/config_dir/nn_config).absolute()} -l {(project_root/log_dir).absolute()}'
+            f'{os.getenv("TERM")} -e {(project_root/NetworkCallController/exec_dir/NetworkCallController).absolute()} -c {(project_root/NetworkCallController/config_dir/ncc_config).absolute()} -l {(project_root/log_dir).absolute()}'
         )
 
-    for cn_config in cn_configs:
+    for cc_config in ConnectionController_configs:
         os.popen(
-            f'{os.getenv("TERM")} -e {(project_root/cn/exec_dir/cn).absolute()} -c {(project_root/cn/config_dir/cn_config).absolute()} -l {(project_root/log_dir).absolute()}'
+            f'{os.getenv("TERM")} -e {(project_root/ConnectionController/exec_dir/ConnectionController).absolute()} -c {(project_root/ConnectionController/config_dir/cc_config).absolute()} -l {(project_root/log_dir).absolute()}'
+        )
+
+    for nn_config in NetworkNode_configs:
+        os.popen(
+            f'{os.getenv("TERM")} -e {(project_root/NetworkNode/exec_dir/NetworkNode).absolute()} -c {(project_root/NetworkNode/config_dir/nn_config).absolute()} -l {(project_root/log_dir).absolute()}'
+        )
+
+    for cn_config in ClientNode_configs:
+        os.popen(
+            f'{os.getenv("TERM")} -e {(project_root/ClientNode/exec_dir/ClientNode).absolute()} -c {(project_root/ClientNode/config_dir/cn_config).absolute()} -l {(project_root/log_dir).absolute()}'
         )
